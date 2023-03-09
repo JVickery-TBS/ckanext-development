15c68371dd4fd3800fc48c21563d3daf898f412a
JVickery-TBS <jesse.vickery@tbs-sct.gc.ca>
2023-03-09 22:02:31 +0000

    feat(dev): schemas and dcat templates;
    - Added schemas for fluent and translatable fields.
    - Added templates to output easy links for dcat.

--- ckanext/development/plugin.py
+++ ckanext/development/plugin.py
@@ -13,4 +13,18 @@ class DevelopmentPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "development")
 
+        config_['scheming.dataset_schemas'] = """
+            ckanext.development:schemas/dataset.yaml
+            """
+
+        config_['scheming.organization_schemas'] = """
+            ckanext.development:schemas/organization.yaml
+            """
+
+        config_['scheming.presets'] = """
+            ckanext.scheming:presets.json
+            ckanext.fluent:presets.json
+            ckanext.development:schemas/presets.yaml
+            ckanext.development:schemas/validation_placeholder_presets.yaml
+            """
 
